bb9bb25d3bed6c7193da5d54c10f48e3a4bbdccf
Md.Rakib <mdrakibk849@gmail.com>
2023-11-04 02:53:56 +0600

    Profile Photo Uplode and Post Create wihout Admin and Post Create

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailsView, PostCreateView
 
 urlpatterns = [
-    path('', views.homepage, name="home page"),
+    path('', PostListView.as_view(), name="home page"),
+    path('post/<int:pk>', PostDetailsView.as_view(), name="post detail"),
+    path('post/new', PostCreateView.as_view(), name="post create"),
     path('aboutpage/', views.about, name="about page"),
 ]
--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView
 
 
 def homepage(request):
@@ -7,6 +8,29 @@ def homepage(request):
     return render(request, 'home/homepage.html', {"Posts": Posts})
 
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'home/homepage.html'
+    context_object_name = 'Posts'
+    ordering = ['-date_post']
+
+
+class PostDetailsView(DetailView):
+    model = Post
+    template_name = 'home/post_detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'home/post_form.html'
+
+    # template_name = 'home/post_detail.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 def about(request):
     return render(request, 'home/about.html', {"title": "About page"})
-
