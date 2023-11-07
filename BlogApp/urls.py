c79bca0ef2958a200872d0b7465d8a3b9dc226b6
Md.Rakib <mdrakibk849@gmail.com>
2023-11-07 21:33:29 +0600

    Post Updete Delete Create

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailsView, PostCreateView
+from .views import PostListView, PostDetailsView, PostCreateView, PostUpdateView,PostDeleteView
 
 urlpatterns = [
     path('', PostListView.as_view(), name="home page"),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="post delete"),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name="post update"),
     path('post/<int:pk>', PostDetailsView.as_view(), name="post detail"),
     path('post/new', PostCreateView.as_view(), name="post create"),
     path('aboutpage/', views.about, name="about page"),
--- BlogApp/views.py
+++ BlogApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
 def homepage(request):
@@ -30,6 +31,34 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'home/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+    template_name = 'home/post_delete.html'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
